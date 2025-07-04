946550febf71ffd4d1898746e45049d97e0ed5c4
Manish-Bhusal <manishbhusal180@gmail.com>
2025-07-04 21:12:03 +0545

    Delete q3.py

--- q3.py
+++
@@ -1,9 +0,0 @@
-number = int(input("Enter a number to find its factorial: "))
-print(f"You have entered: {number}")
-factorial = 1
-
-while number > 1:
-    factorial *= number
-    number -= 1         
-
-print(f"The factorial {number}! of the number is {factorial}.")
